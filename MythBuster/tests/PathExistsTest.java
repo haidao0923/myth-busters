5bdbe5171acf517109e737cd95b936ea36c29d2c
Rishabh Jain <r.jain.voyager@gmail.com>
2021-03-14 11:50:50 -0500

    update path test to separate unit tests

--- MythBuster/tests/PathExistsTest.java
+++ MythBuster/tests/PathExistsTest.java
@@ -14,6 +14,7 @@ import static org.junit.Assert.assertTrue;
 import static org.testfx.api.FxAssert.verifyThat;
 public class PathExistsTest extends ApplicationTest {
     private Controller controller;
+    private int counter = 0;
     @Override
     public void start(Stage primaryStage) throws Exception {
         controller = new Controller();
@@ -29,7 +30,6 @@ public class PathExistsTest extends ApplicationTest {
         Stack<Room> rooms = new Stack<>();
         boolean[][] visited = new boolean[6][6];
         Room curr;
-        int counter = 0;
         rooms.push(controller.getCurrentRoom());
         boolean passed = false; //default to fail
         while (!rooms.isEmpty()) {
@@ -38,7 +38,6 @@ public class PathExistsTest extends ApplicationTest {
             controller.setCurrentRoom(curr);
             if (curr instanceof BossRoom) {
                 passed = true;
-                assertTrue(counter >= 6); //make sure path is long enough
             }
             if (curr.getBottomDoor() != null && !visited[curr.getBottomDoor().getDestination().getRow()]
                     [curr.getBottomDoor().getDestination().getColumn()]) {
@@ -67,5 +66,49 @@ public class PathExistsTest extends ApplicationTest {
             visited[curr.getRow()][curr.getColumn()] = true;
         }
         assertTrue(passed);
+    }
+
+    @Test
+    public void testPathLength() {
+        clickOn("#HeroNameTextField");
+        write("this is a valid name");
+        clickOn("Begin!");
+        Stack<Room> rooms = new Stack<>();
+        boolean[][] visited = new boolean[6][6];
+        Room curr;
+        rooms.push(controller.getCurrentRoom());
+        while (!rooms.isEmpty()) {
+            counter++;
+            curr = rooms.pop();
+            controller.setCurrentRoom(curr);
+            if (curr instanceof BossRoom) {
+                assertTrue(counter >= 6);
+            }
+            if (curr.getBottomDoor() != null && !visited[curr.getBottomDoor().getDestination().getRow()]
+                    [curr.getBottomDoor().getDestination().getColumn()]) {
+                rooms.push(curr.getBottomDoor().getDestination());
+                visited[curr.getBottomDoor().getDestination().getRow()]
+                        [curr.getBottomDoor().getDestination().getColumn()] = true;
+            }
+            if (curr.getTopDoor() != null && !visited[curr.getTopDoor().getDestination().getRow()]
+                    [curr.getTopDoor().getDestination().getColumn()]) {
+                rooms.push(curr.getTopDoor().getDestination());
+                visited[curr.getTopDoor().getDestination().getRow()]
+                        [curr.getTopDoor().getDestination().getColumn()] = true;
+            }
+            if (curr.getRightDoor() != null && !visited[curr.getRightDoor().getDestination().getRow()]
+                    [curr.getRightDoor().getDestination().getColumn()]) {
+                rooms.push(curr.getRightDoor().getDestination());
+                visited[curr.getRightDoor().getDestination().getRow()]
+                        [curr.getRightDoor().getDestination().getColumn()] = true;
+            }
+            if (curr.getLeftDoor() != null && !visited[curr.getLeftDoor().getDestination().getRow()]
+                    [curr.getLeftDoor().getDestination().getColumn()]) {
+                rooms.push(curr.getLeftDoor().getDestination());
+                visited[curr.getLeftDoor().getDestination().getRow()]
+                        [curr.getLeftDoor().getDestination().getColumn()] = true;
+            }
+            visited[curr.getRow()][curr.getColumn()] = true;
+        }
     }
 }
