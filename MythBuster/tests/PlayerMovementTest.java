57d850412dad5e7863b31bc6dd93691455f0c38c
hdao35 <hdao35@gatech.edu>
2021-03-13 23:47:44 -0500

    Fix player movement to move in two diagonal. Add two unit test for moving player in one and two direction respectively.

---
+++ MythBuster/tests/PlayerMovementTest.java
@@ -0,0 +1,171 @@
+package tests;
+
+import controller.Controller;
+import gamefiles.Player;
+import javafx.scene.input.KeyCode;
+import javafx.stage.Stage;
+
+import org.junit.Test;
+import org.testfx.framework.junit.ApplicationTest;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class PlayerMovementTest extends ApplicationTest {
+    private Controller controller;
+    private Player player;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        controller = new Controller();
+        controller.start(primaryStage);
+        Controller.goToConfigurationScreen();
+    }
+
+    /**
+     * Make sure that pressing the correct buttons correspond to the correct direction of movement.
+     */
+    @Test
+    public void oneDirectionalMovement() {
+        String name = "Test";
+        clickOn("#HeroNameTextField");
+        write(name);
+        clickOn("Begin!");
+        player = controller.getPlayer();
+        double previousPositionX = player.getPositionX();
+        double previousPositionY = player.getPositionY();
+        System.out.println(previousPositionX);
+        press(KeyCode.A);
+        assertEquals(10, player.getSpeed(), 0);
+        assertTrue(previousPositionX > player.getPositionX());
+        release(KeyCode.A);
+        previousPositionX = player.getPositionX();
+        previousPositionY = player.getPositionY();
+
+        press(KeyCode.W);
+        assertEquals(10, player.getSpeed(), 0);
+        assertTrue(previousPositionY > player.getPositionY());
+        release(KeyCode.W);
+        previousPositionX = player.getPositionX();
+        previousPositionY = player.getPositionY();
+
+        press(KeyCode.S);
+        assertEquals(10, player.getSpeed(), 0);
+        assertTrue(previousPositionY < player.getPositionY());
+        release(KeyCode.S);
+        previousPositionX = player.getPositionX();
+        previousPositionY = player.getPositionY();
+
+        press(KeyCode.D);
+        assertEquals(10, player.getSpeed(), 0);
+        assertTrue(previousPositionX < player.getPositionX());
+        release(KeyCode.D);
+        previousPositionX = player.getPositionX();
+        previousPositionY = player.getPositionY();
+
+        press(KeyCode.LEFT);
+        assertEquals(10, player.getSpeed(), 0);
+        assertTrue(previousPositionX > player.getPositionX());
+        release(KeyCode.LEFT);
+        previousPositionX = player.getPositionX();
+        previousPositionY = player.getPositionY();
+
+        press(KeyCode.UP);
+        assertEquals(10, player.getSpeed(), 0);
+        assertTrue(previousPositionY > player.getPositionY());
+        release(KeyCode.UP);
+        previousPositionX = player.getPositionX();
+        previousPositionY = player.getPositionY();
+
+        press(KeyCode.DOWN);
+        assertEquals(10, player.getSpeed(), 0);
+        assertTrue(previousPositionY < player.getPositionY());
+        release(KeyCode.DOWN);
+        previousPositionX = player.getPositionX();
+        previousPositionY = player.getPositionY();
+
+        press(KeyCode.RIGHT);
+        assertEquals(10, player.getSpeed(), 0);
+        assertTrue(previousPositionX < player.getPositionX());
+        release(KeyCode.RIGHT);
+        previousPositionX = player.getPositionX();
+        previousPositionY = player.getPositionY();
+    }
+
+    /**
+     * Make sure that if the player move in two directions at once, that the speed is reduced to 7.
+     */
+    @Test
+    public void twoDirectionalMovement() {
+        String name = "Test";
+        clickOn("#HeroNameTextField");
+        write(name);
+        clickOn("Begin!");
+        player = controller.getPlayer();
+        double previousPositionX = player.getPositionX();
+        double previousPositionY = player.getPositionY();
+        press(KeyCode.A, KeyCode.W);
+        assertEquals(7, player.getSpeed(), 0);
+        assertTrue(previousPositionX > player.getPositionX());
+        assertTrue(previousPositionY > player.getPositionY());
+        release(KeyCode.A, KeyCode.W);
+        previousPositionX = player.getPositionX();
+        previousPositionY = player.getPositionY();
+
+        press(KeyCode.LEFT, KeyCode.UP);
+        assertEquals(7, player.getSpeed(), 0);
+        assertTrue(previousPositionX > player.getPositionX());
+        assertTrue(previousPositionY > player.getPositionY());
+        release(KeyCode.LEFT, KeyCode.UP);
+        previousPositionX = player.getPositionX();
+        previousPositionY = player.getPositionY();
+
+        press(KeyCode.A, KeyCode.S);
+        assertEquals(7, player.getSpeed(), 0);
+        assertTrue(previousPositionX > player.getPositionX());
+        assertTrue(previousPositionY < player.getPositionY());
+        release(KeyCode.A, KeyCode.S);
+        previousPositionX = player.getPositionX();
+        previousPositionY = player.getPositionY();
+
+        press(KeyCode.LEFT, KeyCode.DOWN);
+        assertEquals(7, player.getSpeed(), 0);
+        assertTrue(previousPositionX > player.getPositionX());
+        assertTrue(previousPositionY < player.getPositionY());
+        release(KeyCode.LEFT, KeyCode.DOWN);
+        previousPositionX = player.getPositionX();
+        previousPositionY = player.getPositionY();
+
+        press(KeyCode.D, KeyCode.W);
+        assertEquals(7, player.getSpeed(), 0);
+        assertTrue(previousPositionX < player.getPositionX());
+        assertTrue(previousPositionY > player.getPositionY());
+        release(KeyCode.D, KeyCode.W);
+        previousPositionX = player.getPositionX();
+        previousPositionY = player.getPositionY();
+
+        press(KeyCode.RIGHT, KeyCode.UP);
+        assertEquals(7, player.getSpeed(), 0);
+        assertTrue(previousPositionX < player.getPositionX());
+        assertTrue(previousPositionY > player.getPositionY());
+        release(KeyCode.RIGHT, KeyCode.UP);
+        previousPositionX = player.getPositionX();
+        previousPositionY = player.getPositionY();
+
+        press(KeyCode.D, KeyCode.S);
+        assertEquals(7, player.getSpeed(), 0);
+        assertTrue(previousPositionX < player.getPositionX());
+        assertTrue(previousPositionY < player.getPositionY());
+        release(KeyCode.D, KeyCode.S);
+        previousPositionX = player.getPositionX();
+        previousPositionY = player.getPositionY();
+
+        press(KeyCode.RIGHT, KeyCode.DOWN);
+        assertEquals(7, player.getSpeed(), 0);
+        assertTrue(previousPositionX < player.getPositionX());
+        assertTrue(previousPositionY < player.getPositionY());
+        release(KeyCode.RIGHT, KeyCode.DOWN);
+        previousPositionX = player.getPositionX();
+        previousPositionY = player.getPositionY();
+    }
+}
