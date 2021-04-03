4bff54b0570230a2b5c96c7dcb0149ac13d44461
dyuan43 <dyuan@gatech.edu>
2021-04-03 08:01:47 -0400

    Add Item abstract class, Consumable abstract class, HealthPotion, and RagePotion. Put AnimationTimer item ideas in updateItems() in Player.java.

---
+++ MythBuster/gamefiles/items/Consumable.java
@@ -0,0 +1,53 @@
+package gamefiles.items;
+
+public abstract class Consumable extends Item {
+    private long duration;
+    private long durationTimer;
+    private boolean consumed;
+
+    public Consumable(int id, String name, String description, int quantity, boolean active, long duration) {
+        super(id, name, description, quantity, active);
+        this.duration = duration;
+        this.durationTimer = 0;
+        this.consumed = false;
+    }
+
+    public void use() {
+        if (!this.isConsumed()) {
+            durationTimer = this.getDuration();
+            this.subtractQuantity(1);
+            this.toggleConsumed();
+        }
+    }
+
+    public boolean update() {
+        if (this.durationTimer == 0) {
+            this.toggleConsumed();
+            this.toggleActive();
+            return false; // ended; "no update"
+        } else {
+            durationTimer--;
+            return true; // not ended; "updated"
+        }
+    }
+
+    public long getDuration() {
+        return this.duration;
+    }
+
+    public long getDurationTimer() {
+        return this.durationTimer;
+    }
+
+    public void toggleConsumed() {
+        if (this.consumed) {
+            this.consumed = false;
+        } else {
+            this.consumed = true;
+        }
+    }
+
+    public boolean isConsumed() {
+        return this.consumed;
+    }
+}
---
+++ MythBuster/gamefiles/items/RagePotion.java
@@ -0,0 +1,37 @@
+package gamefiles.items;
+
+import controller.Controller;
+import gamefiles.characters.Player;
+
+public class RagePotion extends Consumable {
+
+    // String name = "Rage Potion";
+    // String description = "Doubles your attack output for 10 seconds.";
+    // int quantity = 0;
+    // long duration = 600;
+    private boolean doubled;
+    private double oldDamage;
+
+    public RagePotion(int id, String name, String description, int quantity, boolean active, long duration) {
+        super(id, name, description, quantity, active, duration);
+    }
+
+    public void effect(long currentNanoTime) {
+        use();
+        
+        if (!doubled) {
+            Player player = Controller.getPlayer();
+            oldDamage = player.getDamageStat();
+            player.addDamageStat(oldDamage);
+            doubled = true;
+        }
+
+        boolean updated = update();
+        
+        if (!updated) {
+            Player player = Controller.getPlayer();
+            player.subtractDamageStat(oldDamage);
+            doubled = false;
+        }
+    }
+}
