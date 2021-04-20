fb859ad349249f608403a0b4ee4de6a16f250d57
Rishabh Jain <rjain343@gatech.edu>
2021-04-19 21:18:23 -0500

    remove old boss square from boss room

--- MythBuster/gamefiles/rooms/BossRoom.java
+++ MythBuster/gamefiles/rooms/BossRoom.java
@@ -14,33 +14,8 @@ public class BossRoom extends Room {
 
     public BossRoom(int width, int height, int row, int column) {
         super(width, height, row, column);
-        boss = new Rectangle(width / 2 + 150, height / 2, 130, 130);
-        boss.setFill(Color.BLACK);
-        boss.setId("boss");
-        bossName = new Text(width / 2 + 155, height / 2 + 50, "The Boss");
-        bossName.setFill(Color.WHITE);
-        bossName.setStyle("-fx-font-size: 30;");
-
-
-        boss.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent mouseEvent) {
-                Controller.goToWinScreen();
-            }
-        });
-        bossName.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent mouseEvent) {
-                Controller.goToWinScreen();
-            }
-        });
-
-        boss.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseEvent -> Controller.goToWinScreen());
-
-
     }
 
-    //this class will have a method to generate a boss, as well as trigger a victory condition
     @Override
     public Group getRoomGroup() {
         Group roomGroup = super.getRoomGroup();
