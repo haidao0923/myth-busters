8ce5fd7069b2070b8a947d83233185b48a7e9782
Adithya Vasudev <adithya.gvasudev@gmail.com>
2021-04-26 16:34:36 -0400

    Bug Fix on Dropped Item

--- MythBuster/gamefiles/items/DroppedItem.java
+++ MythBuster/gamefiles/items/DroppedItem.java
@@ -164,7 +164,7 @@ public class DroppedItem implements Droppable {
         Label display = new Label(text);
         display.setPrefWidth(Controller.getW());
         display.setStyle("-fx-font-size: 30; -fx-font-weight: bold; -fx-text-fill: white;"
-                + "-fx-alignment:CENTER;");
+                + "-fx-alignment:CENTER; -fx-font-family: Papyrus");
         display.setLayoutY(200);
         display.setId("dropNotificationDisplay");
         Controller.getGameScreen().getBoard().getChildren().add(display);
